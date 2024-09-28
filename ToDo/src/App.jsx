ad85d4d30b49dfe983fbbe7c227a87c262be66d5
Amarjeet <amarjeetmistri41@gmail.com>
2024-09-28 23:55:41 +0530

    Add

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/Theme";
+import ThemeChangeBtn from "./components/ThemeChangeBtn";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [themeMode, setThemeMode] = useState("dark");
 
-  return <></>;
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    
+
+    // Remove any existing theme classes
+    html.classList.remove("light", "dark");
+
+    // Add the new theme class
+    html.classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ darkTheme, lightTheme, themeMode }}>
+      <div className="dark:bg-[#282828] dark:text-white text-black w-full h-screen fixed top-0 left-0 bg-[#b6b6b6]">
+        <div className=" absolute w-[10px] top-2 right-[60px]">
+          <ThemeChangeBtn />
+        </div>
+        <div className="w-full h-screen  flex justify-center items-center">
+          <Card classList=" rounded" />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
